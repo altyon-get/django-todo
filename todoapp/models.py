0c723df8ec2ecd7964690f112a76ae006a908a68
altyon_get <shubhamojha4444@gmail.com>
2024-06-26 17:08:39 +0530

    added todo model

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Todo(models.Model):
+    LOW = 'L'
+    HIGH = 'H'
+    MEDIUM = 'M'
+    PRIORITY_CHOICES = (
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        (HIGH, 'High'),
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+
+class TodoListCreateView(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
